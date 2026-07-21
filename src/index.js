088fa059454615c8d7ba6dc8f95f3c5d51863345
Abdullah Rashid <229150035+abdullahrashid359@users.noreply.github.com>
2026-07-21 14:15:44 +0500

    Complete restaurant page functionality with dynamic tab navigation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import { homepage } from "./homepage.js";
 import { menu } from "./menu.js";
 import { about } from "./about.js";
+
+const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const aboutBtn = document.querySelector("#about-btn");
+const contentDiv = document.querySelector("#content");
+
+const renderPage = function(page) {
+    contentDiv.textContent = "";
+    page();
+}
+
+homeBtn.addEventListener("click" , () => renderPage(homepage));
+
+menuBtn.addEventListener("click" , () => renderPage(menu));
+
+aboutBtn.addEventListener("click" , () => renderPage(about));
+
 homepage();
